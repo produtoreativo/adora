ee6279b211eb8692e17176f4002acfab41c63f12
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2022-03-04 22:14:46 -0300

    chores:  Add built-in Swagger support
    closes #9

--- src/domain/contexts/application/application.controller.ts
+++ src/domain/contexts/application/application.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { Application } from '../..//entities/application.entity';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller()
 export class ApplicationController {
   constructor(private readonly applicationService: ApplicationService) {}
 
+  @ApiBody({ type: Application })
   @Post('application')
   async createApplication(
     @Body() application: Application,
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
@@ -7,6 +9,16 @@ async function bootstrap() {
     credentials: true,
     origin: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('ADORA Metrics')
+    .setDescription('API documentation')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs/api', app, document);
+
   await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
