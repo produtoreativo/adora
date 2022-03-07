e7f3fe34c664cf6b2b4cec4ea15519eb9644661c
Christiano Milfont <cmilfont@gmail.com>
2022-03-07 09:15:38 -0300

    feat(#18): criando métodos genéricos e fechando o fluxo do Leadtime com deploy

---
+++ src/domain/dtos/GithubDeploy.dto.ts
@@ -0,0 +1,12 @@
+
+class GithubDeploy {
+  deployment_status: {
+    state: string;
+  };
+  data: {
+    name: string;
+    origin: string;
+    state: string;
+  };
+}
+export default GithubDeploy;
