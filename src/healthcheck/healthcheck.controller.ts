f5e5c14dc4d0b80bbf3e530d243afcbfa478ca49
Christiano Milfont <cmilfont@gmail.com>
2022-09-15 08:51:36 -0300

    versao lambda

---
+++ src/healthcheck/healthcheck.controller.ts
@@ -0,0 +1,32 @@
+
+
+import { Controller, Get } from '@nestjs/common';
+import { get } from 'http';
+// import { 
+//   HealthCheckService, 
+//   HttpHealthIndicator, 
+//   HealthCheck 
+// } from '@nestjs/terminus';
+
+@Controller()
+export class HealthcheckController {
+  constructor(
+    // private health: HealthCheckService,
+    // private http: HttpHealthIndicator,
+  ) {}
+
+  @Get()
+  // @HealthCheck()
+  check() {
+    return {
+      status: 'ok ECS'
+    };
+  }
+
+  @Get('hello/en')
+  getHello() {
+    return {
+      status: "EN de Sobral"
+    };
+  }
+}
